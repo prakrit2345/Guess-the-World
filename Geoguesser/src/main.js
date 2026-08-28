536c9ec72d2da4d0bd72eb70ec7aface4c4d1caf
Milan <bistmilan46@gmail.com>
2026-08-28 16:12:07 +0545

    Merge conflict fixed

--- Geoguesser/src/main.js
+++ Geoguesser/src/main.js
@@ -7,4 +7,4 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
