87a658249bf7b87d0cde7879beec1276e7bb1a35
prakrit-devkota <devkotaprakrit9@gmail.com>
2026-09-03 08:27:14 +0545

    backend from authentication

--- Backend/apps/users/models.py
+++ Backend/apps/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False, unique=True)
+    password = models.CharField(max_length=200, blank=False)
